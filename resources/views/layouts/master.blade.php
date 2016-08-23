65d0d4cd4ee8794acb0a0df958dcb15d2eb99171
Leonel Barrera <leodota7@gmail.com>
2016-08-22 22:02:18 -0600

    Create folder layouts

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <title>UBIMOV - @yield('title')</title>
+
+    @yield('css')
+
+</head>
+<body>
+    @section('navbar')
+
+        <div class="container-fluid">
+            @yield('content')
+        </div>
+
+    @yield('javascript')
+
+    @section('footer')
+</body>
+</html>
