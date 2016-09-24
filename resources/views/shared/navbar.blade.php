4dde34e108b489ba4dccd6ea1dde0074d2f3bee3
Leonel Barrera <leodota7@gmail.com>
2016-09-24 16:48:53 -0600

    create NAVBAR

--- resources/views/shared/navbar.blade.php
+++ resources/views/shared/navbar.blade.php
@@ -1,6 +1,5 @@
 <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
     <div class="container-fluid">
-        <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
                 <span class="sr-only">Toggle navigation</span>
@@ -8,26 +7,46 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="{{ Request::url() }}">UBIMOV - GPS Tracking</a>
+            <a class="navbar-brand" href="{{ Request::url() }}">UBIMOV</a>
         </div>
 
-        <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse navbar-ex1-collapse">
             <ul class="nav navbar-nav navbar-right">
-                <li class="active"><a href="{{ url('/') }}">
-                <i class="fa fa-home fa-lg fa-fw" aria-hidden="true"></i>
-                Inicio</a></li>
-                <li><a href="#">
-                <i class="fa fa-flask fa-lg fa-fw" aria-hidden="true"></i>
-                Proyecto</a></li>
-                <li><a href="#">
-                <i class="fa fa-users fa-lg fa-fw" aria-hidden="true"></i>
-                Staff</a></li>
-                <li><a href="#">
-                <i class="fa fa-envelope-o fa-lg fa-fw" aria-hidden="true"></i>
-                Contacto</a></li>
+                <li class="{{ Request::path() ==  '/' ? 'active' : ''  }}">
+                    <a href="{{ url('/') }}">
+                    <i class="fa fa-home fa-lg fa-fw" aria-hidden="true"></i>
+                        Inicio
+                    </a>
+                </li>
+
+                <li class="{{ Request::path() ==  'project' ? 'active' : ''  }}">
+                    <a href="{{ url('project') }}">
+                    <i class="fa fa-flask fa-lg fa-fw" aria-hidden="true"></i>
+                        Proyecto
+                    </a>
+                </li>
+
+                <li class="{{ Request::path() ==  'gps' ? 'active' : ''  }}">
+                    <a href="{{ url('gps') }}">
+                    <i class="fa fa-location-arrow fa-lg fa-fw" aria-hidden="true"></i>
+                        Demo GPS-Tracking
+                    </a>
+                </li>
+
+                <li class="{{ Request::path() ==  'staff' ? 'active' : ''  }}">
+                    <a href="{{ url('staff') }}">
+                    <i class="fa fa-users fa-lg fa-fw" aria-hidden="true"></i>
+                        Staff
+                    </a>
+                </li>
+
+                <li class="{{ Request::path() ==  'contact' ? 'active' : ''  }}">
+                    <a href="{{ url('contact') }}">
+                    <i class="fa fa-envelope-o fa-lg fa-fw" aria-hidden="true"></i>
+                        Contacto
+                    </a>
+                </li>
             </ul>
         </div>
-        <!-- /.navbar-collapse -->
     </div>
 </nav>
