438450c826376cf8c41e378949b8c72d3e137b2b
Leonel Barrera <leodota7@gmail.com>
2016-09-24 16:47:51 -0600

    Create layout master

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <title>UBIMOV - @yield('title')</title>
-
+    <link rel="icon" type="image/png" href="/img/favicon-96x96.png" />
     @yield('css')
 
 </head>
